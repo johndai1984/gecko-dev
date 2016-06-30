2adc868a0e73e2f79c61d3fce309b5017edcc4b9
John Dai <jdai@mozilla.com>
2016-06-30 15:27:48 +0800

    Bug 1264323 - Set namespace when DOMParser.parseFromString(application/xhtml+xml). r=smaug

--- dom/base/nsDocument.cpp
+++ dom/base/nsDocument.cpp
@@ -13397,6 +13397,11 @@ nsIDocument::SetCachedEncoder(already_AddRefed<nsIDocumentEncoder> aEncoder)
 void
 nsIDocument::SetContentTypeInternal(const nsACString& aType)
 {
+  if (!IsHTMLOrXHTML() && mDefaultElementType == kNameSpaceID_None &&
+      aType.EqualsLiteral("application/xhtml+xml")) {
+    mDefaultElementType = kNameSpaceID_XHTML;
+  }
+
   mCachedEncoder = nullptr;
   mContentType = aType;
 }
